b4e23c75a6a5e17dc5fc5d09519400b9d5c9c9d6
Frahanz <farhanrahmat321@gmail.com>
2023-11-25 11:33:40 +0700

    first commit

---
+++ looping.js
@@ -0,0 +1,7 @@
+for (let i=0; i <20; i++){
+    console.log("<p>Saya berada dikelas kafekoding hari ke-"+ i + "</p>")
+}
+
+for (let k=0; k <15; k++){
+    console.log("<p>Saya belajar ke-"+ k + "</p>")
+}
